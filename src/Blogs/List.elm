84b6d7a4c14471466ce4788fd01fb02a2dda5e99
Stephen Cate <vindicator135@gmail.com>
2018-05-31 08:06:48 +1000

    Added link(s) view

--- src/Blogs/List.elm
+++ src/Blogs/List.elm
@@ -1,7 +1,7 @@
 module Blogs.List exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (style)
+import Html.Attributes exposing (style, href)
 import Blogs.Messages exposing (Msg(..))
 import Blogs.Models exposing (Blog)
 
@@ -13,8 +13,10 @@ cellStyle =
     ]
 
 
-
-cell : (List (Attribute msg) -> a -> b) -> a -> b-- make a cell (th or td) with the pre-set style attribute
+cell :
+    (List (Attribute msg) -> a -> b)
+    -> a
+    -> b -- make a cell (th or td) with the pre-set style attribute
 cell el children =
     el [ style cellStyle ] children
 
@@ -22,15 +24,20 @@ cell el children =
 view : List Blog -> Html Msg
 view blogs =
     div []
-        [ table []
-            [ thead []
-                [ cell th [ text "ID" ]
-                , cell th [ text "Title" ]
-                , cell th [ text "Content" ]
-                , cell th [ text "Published" ]
-                ]
-            , tbody [] <| List.map blogView <| blogs
+        [ linksView blogs
+        ]
+
+
+blogsListView : List Blog -> Html Msg
+blogsListView blogs =
+    table []
+        [ thead []
+            [ cell th [ text "ID" ]
+            , cell th [ text "Title" ]
+            , cell th [ text "Content" ]
+            , cell th [ text "Published" ]
             ]
+        , tbody [] <| List.map blogView <| blogs
         ]
 
 
@@ -41,4 +48,24 @@ blogView blog =
         , cell td [ text blog.title ]
         , cell td [ text blog.content ]
         , cell td [ text blog.published ]
+        ]
+
+
+linksView : List Blog -> Html Msg
+linksView blogs =
+    table []
+        [ thead []
+            [ cell th [ text "ID" ]
+            , cell th [ text "Title" ]
+            , cell th [ text "" ] ]
+        , tbody [] <| List.map linkView <| blogs
+        ]
+
+
+linkView : Blog -> Html Msg
+linkView blog =
+    tr []
+        [ cell td [ text <| toString blog.blogId ]
+        , cell td [ text <| toString blog.title ]
+        , cell td [ a [ href "/123123", title "View"] [] ]
         ]
--- src/Models.elm
+++ src/Models.elm
@@ -9,16 +9,20 @@ type alias Model =
     { blogs : List Blog
     }
 
-emptyModel : Model
-emptyModel = { blogs = [] }
 
-init : ( Model, Cmd Msg)
+emptyModel : Model
+emptyModel =
+    { blogs = [] }
+
+
+init : ( Model, Cmd Msg )
 init =
     let
-        model = 
-            Model []
+        model =
+            Model [ Blog 1 "3 Tips to successfully land your IELTS" "The International English Language Testing System (IELTS) is one among many great feats every aspiring migrant gets to hurdle with. It will definitely greet you on your way along your visa journey. If you're like me, you may have had that same inkling to just curse upon this irksome requirement. But curse as we might, IELTS is there to stay and we just have to do it." "03 May 2016" ]
+
         cmds =
             Cmd.batch
                 [ Cmd.map BlogMsg Blogs.Commands.fetchAll ]
     in
-        ( model, cmds)
+        ( model, cmds )
