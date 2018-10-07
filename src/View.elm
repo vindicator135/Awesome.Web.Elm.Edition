815fec78d0bd1bfae47dae1f022dfc7039824e1c
vindicator135 <vindicator135@gmail.com>
2018-10-07 21:53:47 +1100

    Fixed broken merge changes

--- src/View.elm
+++ src/View.elm
@@ -9,34 +9,40 @@ import Blogs.View
 import Blogs.Models exposing (..)
 import Routing exposing (..)
 import Blogs.View exposing (..)
-
 import Browser exposing (Document)
 
 
 view : Model -> Document Msg
 view model =
-<<<<<<< HEAD
     { title = "A Journey To Awesome"
-    , body = [ (headerView model)
-             , (mainContentsView model)
-             , footerView
-             ]
+    , body =
+        [ (mainContentsView model)
+        ]
     }
-        
-=======
-    let
-        section contents =
-            div [ class "container" ]
-                [ div [ class "row" ]
-                    contents
-                ]
->>>>>>> a6ba63c99b2155744db643c2466a1b0065e526c2
 
+
+mainContentsView : Model -> Html Msg
+mainContentsView model =
+    let
         banner =
             [ div [ class "logo-image-300w-102h mt-3 mb-3" ] [] ]
 
         header blogs =
             [ Html.map BlogMsg <| Blogs.List.view blogs ]
+
+        title =
+            div [ class "container" ]
+                [ div [ class "row" ]
+                    [ div
+                        [ class "col-sm-6"
+                        , style "background" "url(\"\\images\\custom\\main-logo-300x102.png\")"
+                        , style "height" "102px"
+                        ]
+                        []
+                    , div [ class "col-sm-6" ]
+                        []
+                    ]
+                ]
 
         body route blogs =
             case route of
@@ -52,47 +58,14 @@ view model =
                     ]
 
                 NotFoundRoute ->
-<<<<<<< HEAD
-                    div [ class "col-sm-8" ] [ text "Route not defined" ]
-
-        x = Debug.log "mainContentsView" model
-
-        rightContents =
-            div [ class "col-sm-4" ]
-                [ text "Right content" ]
-    in
-        div [ class "container" ]
-            [ div [ class "row" ]
-                [ centerContents
-                , rightContents
-                ]
-            ]
-
-
-headerView : Model -> Html Msg
-headerView model =
-    let
-        title =
-            div [ class "container" ]
-                [ div [ class "row" ]
-                    [ div [ class "col-sm-6"
-                          , style "background" "url(\"\\images\\custom\\main-logo-300x102.png\")"
-                          , style "height" "102px" ]
-                        []
-                    , div [ class "col-sm-6" ]
-                        []
-                    ]
-                ]
-=======
-                    [ text "Route not defined" ]
->>>>>>> a6ba63c99b2155744db643c2466a1b0065e526c2
+                    [ div [ class "col-sm-8" ] [ text "Route not defined" ] ]
 
         footer =
             []
     in
         div []
-            [ section <| banner
-            , section <| header model.blogs
-            , section <| [ div [ class "col-sm-12" ] <| body model.route model.blogs ]
-            , section <| footer
+            [ section [] <| banner
+            , section [] <| header model.blogs
+            , section [] <| [ div [ class "col-sm-12" ] <| body model.route model.blogs ]
+            , section [] <| footer
             ]
